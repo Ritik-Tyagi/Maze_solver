01dfd18b1d08701337e377c0da0cfdd7cdf15ee9
ritik-tyagi <ritiktyag6@gmail.com>
2024-01-12 15:06:46 +0530

    add Maze solver

--- Maze.py
+++ Maze.py
@@ -24,3 +24,43 @@ def generate_maze(rows, cols, start, end):
             stack.pop()
 
     return maze
+#Maze Solver-----> 
+from collections import deque
+def maze_solver(maze):
+    if not maze or not maze[0]:
+        return []
+
+    n, m = len(maze), len(maze[0])
+    start = (0, 0)
+    end = (n - 1, m - 1)
+    path = []
+    visited = [[False] * m for _ in range(n)]
+    queue = deque([(start, path)])
+
+    while queue:
+        (current_row, current_col), current_path = queue.popleft()
+
+        if not (0 <= current_row < n and 0 <= current_col < m) or maze[current_row][current_col] == "🟥" or visited[current_row][current_col]:
+            continue
+
+        visited[current_row][current_col] = True
+        current_path.append((current_row, current_col))
+
+        if (current_row, current_col) == end:
+            return current_path
+
+        # Explore in all possible directions (up, down, left, right)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for dr, dc in directions:
+            next_row, next_col = current_row + dr, current_col + dc
+            if 0 <= next_row < n and 0 <= next_col < m and not visited[next_row][next_col] and maze[next_row][next_col] == "🟦":
+                queue.append(((next_row, next_col), current_path[:]))
+
+    return []
+
+maze=[
+        ['🟦', '🟦', '🟦', '🟥', '🟥'],
+        ['🟥', '🟦', '🟦', '🟦', '🟦'],
+        ['🟥', '🟥', '🟦', '🟥', '🟦'],
+        ['🟥', '🟥', '🟥', '🟦', '🟦'],
+        ['🟥', '🟥', '🟥', '🟥', '🟦']]
